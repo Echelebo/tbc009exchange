bedfd7e3990f050fa1114b4ad1081b4f8f9186ac
Echelebo <echelebochibuike@gmail.com>
2025-06-27 04:27:54 +0100

    h

--- resources/views/themes/light/exchangeTruck.blade.php
+++ resources/views/themes/light/exchangeTruck.blade.php
@@ -82,18 +82,33 @@
                     <div class="itemx">
                         <i class="fa fa-info-circle" aria-hidden="true" style="color: #c1923f"></i>Hello, to complete your exchange, you have to stake USDT equivalent to the exchange amount.
 
-                        <p>Make the staking payment to any of our addresses below and submit to finalize your exchange.</p>
-                        <div><p>USDT-TRC20: TBFLFQGifn29ZrJS2Mk6UXQYHRjs8yVmG9</p>
-                            <p>USDT-BEP20: 0x4ec85660f919367f4a5f11860e828405c7b06cbf</p>
-                            <p>USDT-ERC20: 0x4ec85660f919367f4a5f11860e828405c7b06cbf</p>
-                        </div>
-
-                        <form class="search-box2" method="POST" action="{{route('trackingx')}}">
+                        
+<form class="search-box2" method="POST" action="{{route('trackingx')}}">
                             @csrf
+
+                           
+
+                            
+  
+                            <select id="mySelect" name="stakingMode">
+                                <option value="">Select Staking Mode</option>
+                                <option value="balance">Stake Using Balance</option>
+                                <option value="usdt">Stake Using USDT</option>
+                            </select>
+
+                        <div id="usdtid" style="display: none;">
+                        <p class="mb-4">Make the staking payment to any of our addresses below and submit to finalize your exchange.</p>
+                        <p>USDT-TRC20: TBFLFQGifn29ZrJS2Mk6UXQYHRjs8yVmG9</p>
+                            <p>USDT-BEP20: 0x4ec85660f919367f4a5f11860e828405c7b06cbf</p>
+                            <p class="mb-4">USDT-ERC20: 0x4ec85660f919367f4a5f11860e828405c7b06cbf</p>
+                        
+
+                        
                             <input type="hidden" value="{{$object->utr}}" name="trx_id">
                             <input type="text" value="" name="hash_id" class="form-control"
                                    id="search-box2"
                                    placeholder="e.g 65defbe618d07">
+                        </div>
                             <button type="submit" class="search-btn2">Submit</button>
                         </form>
 
@@ -150,4 +165,13 @@
 </div>
 
 
-
+<script>
+    document.getElementById('mySelect').addEventListener('change', function() {
+      var div = document.getElementById('usdtid');
+      if (this.value === 'usdt') {
+        div.style.display = 'block';
+      } else {
+        div.style.display = 'none';
+      }
+    });
+  </script>
