9ef60198c7242ab60958d7947c631e86915deb0d
Echelebo <echelebochibuike@gmail.com>
2025-06-28 17:46:08 +0100

    f

--- resources/views/themes/light/exchangeTruck.blade.php
+++ resources/views/themes/light/exchangeTruck.blade.php
@@ -107,7 +107,7 @@
                                    id="search-box2"
                                    placeholder="e.g 65defbe618d07">
                         </div>
-                            <button type="submit" class="search-btn2 mt-4" style="background-color:#c1923f; border-radius: 15px; padding: 10px;">Submit</button>
+                            <button type="submit" class="search-btn2 mt-4 px-8" style="background-color:#c1923f; border-radius: 15px; padding: 10px; font-size: 15px; color: #ffffff; ">Submit</button>
                     </form>
 
                     
