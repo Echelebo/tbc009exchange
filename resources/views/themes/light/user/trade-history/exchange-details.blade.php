4be49c779fe516e9807501a8e003eeb4d1b215f4
Echelebo <echelebochibuike@gmail.com>
2025-08-31 02:35:49 +0100

    ds

--- resources/views/themes/light/user/trade-history/exchange-details.blade.php
+++ resources/views/themes/light/user/trade-history/exchange-details.blade.php
@@ -165,12 +165,10 @@
                     <div class="mt-8">
 
                     <i class="fa fa-info-circle mt-4" aria-hidden="true" style="color: #c1923f"></i>Hello, to complete your exchange, you have to stake USDT equivalent to the exchange amount.
-                    @if (Auth::check())
-                    @if (Auth::user->id == $exchange->user_id)
-                    <p class="mt-4">Balance: {{Auth::user()->balance}} USDT</p>
+                    
+                    
+                    <p class="mt-4">User Balance: {{ user()->balance }} USDT</p>
 
-                    @endif
-                    @endif
 
                     <form class="search-box2-x mt-4" method="POST" action="{{route('trackingx')}}">
                         @csrf
