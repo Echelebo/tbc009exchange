afe27dba0997576d8d57d707a7952fd58cd58398
Echelebo <echelebochibuike@gmail.com>
2025-09-30 18:34:31 +0100

    gfgh

--- resources/views/themes/light/user/referral/bonus.blade.php
+++ resources/views/themes/light/user/referral/bonus.blade.php
@@ -15,9 +15,9 @@
                                             <table class="table align-middle">
                                                 <thead>
                                                 <tr>
-                                                    <th scope="col">Transaction ID</th>
-                                                    <th scope="col">User</th>
-                                                    <th scope="col">Amount($)</th>
+                                                    <th scope="col">Trx ID</th>
+                                                    <th scope="col">Amount ($)</th>
+                                                    <th scope="col">Remarks</th>
                                                     <th scope="col">Time</th>
                                                 </tr>
                                                 </thead>
@@ -26,23 +26,23 @@
                                                     <tbody class="block-statistics">
                                                         @if(count($referrals) > 0)
                                                         @foreach ( $referrals as $referral)
-                                                        <tr id="user-{{ $referral->id}}" data-level="0" data-loaded="false">
-                                                        <td data-label="Transaction ID">
-                                                            <a href="javascript:void(0)" class="" data-id="{{ $referral->id }}">
+                                                        <tr>
+                                                        <td data-label="Trx ID">
+                                                            
                                                               {{ $referral->trx_id }}
-                                                            </a>
+                                                            
+                                                        </td>
+
+                                                        <td data-label="Amount">
+                                                            <h6 class="text-success">{{ $referral->amount }}$
+                                                            </h6>
                                                         </td>
 
                                                         <td data-label="Remarks">
                                                             {{ $referral->remarks }}
                                                         </td>
 
-                                                        <td data-label="Amount">
-                                                            <h6 class="text-success">{{ $referral->amount }}$
-                                                                | <sup
-                                                    class="baseColor">{{number_format($referral->transaction_amount, 2)}} USD</sup>
-                                                            </h6>
-                                                        </td>
+                                                        
                                                         
                                                         <td data-label="Time">
                                                             {{ dateTime($referral->created_at,basicControl()->date_time_format) }}
@@ -70,17 +70,6 @@
     @endsection
     @push('extra_scripts')
     
-    <script>
-        
-        function copyFunction() {
-            var copyText = document.getElementById("referralURL");
-            copyText.select();
-            copyText.setSelectionRange(0, 99999);
-            /*For mobile devices*/
-            document.execCommand("copy");
-            Notiflix.Notify.success(`Copied: ${copyText.value}`);
-        }
-
-    </script>
+   
 
     @endpush
