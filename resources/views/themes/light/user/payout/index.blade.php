c680a37649dafc5903556a5dc2d61bff9ed233fa
Echelebo <echelebochibuike@gmail.com>
2025-11-13 01:16:14 +0100

    ffgfdfd

--- resources/views/themes/light/user/payout/index.blade.php
+++ resources/views/themes/light/user/payout/index.blade.php
@@ -33,7 +33,7 @@
                                <div class=" share_link d-flex align-items-center mt-4">
                                 <p>Choose a payout wallet</p>
                                 <label id="usdttrc20" class="mt-4">USDT TRC-20</label>
-                                    <input type="text" class="input border-0 form-control" style="background-color: #2e403e; color: #ffffff; width: 50%;" id="usdttrc20" value="" readonly="">
+                                    <input type="text" class="input border-0 form-control" style="color: #ffffff; width: 50%;" id="usdttrc20" value="" readonly="">
                                     <button class="copy_btn" onclick="copyFunction()"><i class="fa-regular fa-copy" style="color: #ffffff;"></i>
                                     </button>
                                 </div>
