38b14f7793b87c85ba635e4d85cd9d4441d80852
echelebo <echelebochibuike@gmail.com>
2025-12-19 11:07:06 +0100

    eddsd

--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -506,7 +506,7 @@ class HomeController extends Controller
     {
         $basic = basicControl();
         $userId = Auth::id();
-        $data['users'] = User::where('id', $userId);
+        $data['users'] = User::where('id', $userId)->first();
         $commission = PayoutRequest::where('user_id', $userId)
         ->sum('amount');
         $commissions = PayoutRequest::where('user_id', $userId)
@@ -534,7 +534,7 @@ class HomeController extends Controller
         $basic = basicControl();
         $userId = Auth::id();
 
-        $data['users'] = User::where('id', $userId);
+        $data['users'] = User::where('id', $userId)->first();
         $commission = TopUpRequest::where('user_id', $userId)
         ->sum('amount');
         $commissions = TopUpRequest::where('user_id', $userId)
