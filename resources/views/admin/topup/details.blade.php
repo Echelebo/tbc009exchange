11a71a9e1d2d40d3b714edb10f885b7d024e705b
echelebo <echelebochibuike@gmail.com>
2025-12-20 10:45:54 +0100

    uouoo

--- resources/views/admin/topup/details.blade.php
+++ resources/views/admin/topup/details.blade.php
@@ -1,7 +1,6 @@
 @extends('admin.layouts.app')
-@section('page_title',__('Sell Crypto Details'))
+@section('page_title',__('Top Up Details'))
 @section('content')
-
     <div class="content container-fluid">
         <div class="page-header">
             <div class="row align-items-end">
@@ -11,7 +10,7 @@
                             <li class="breadcrumb-item"><a class="breadcrumb-link"
                                                            href="javascript:void(0);">@lang('Dashboard')</a></li>
                             <li class="breadcrumb-item"><a class="breadcrumb-link"
-                                                           href="javascript:void(0);">@lang('Sell Crypto Details')</a>
+                                                           href="javascript:void(0);">@lang('Top Up')</a>
                             </li>
                             <li class="breadcrumb-item active" aria-current="page">@yield('page_title')</li>
                         </ol>
@@ -20,62 +19,50 @@
                 </div>
             </div>
         </div>
-        @if($sell->status == 2)
+        @if($topup->status == 0)
             <div class="row mx-4">
                 <div class="d-flex justify-content-end gap-2">
-                    <button type="button" class="btn btn-soft-success" id="send" data-bs-target="#confirmation"
-                            data-bs-toggle="modal"><i class="fas fa-paper-plane"></i> @lang("Send")
+                    <button type="button" class="btn btn-soft-success" id="complete" data-bs-target="#confirmation"
+                            data-bs-toggle="modal"><i class="fas fa-check"></i> @lang("Complete")
                     </button>
                     <button type="button" class="btn btn-soft-danger" id="cancel" data-bs-target="#confirmation"
                             data-bs-toggle="modal"><i class="fas fa-times"></i> @lang('Cancel')
                     </button>
-                    @if($sell->refund_wallet)
-                        <button type="button" class="btn btn-soft-secondary" id="refund" data-bs-target="#confirmation"
-                                data-bs-toggle="modal"><i
-                                class="fas fa-arrow-rotate-left"></i> @lang('Refund')
-                        </button>
-                    @endif
                 </div>
             </div>
         @endif
-
         <div class="content container-fluid">
             <div class="row justify-content-lg-center">
                 <div class="col-lg-4">
                     <div class="d-grid gap-3 gap-lg-5">
                         <div class="card">
                             <div class="card-header d-flex justify-content-between">
-                                <h4 class="card-title mt-2">@lang("Trade Information's")</h4>
+                                <h4 class="card-title mt-2">@lang("Top Up Information's")</h4>
                                 <div>
-                                    @if ($sell->status == 2)
-                                        <span class="legend-indicator bg-warning"></span>@lang("Awaiting Complete")
-                                    @elseif ($sell->status == 3)
-                                        <span class="legend-indicator bg-success"></span>@lang("Trade Completed")
-                                    @elseif ($sell->status == 5)
-                                        <span class="legend-indicator bg-danger"></span>@lang("Trade Cancel")
-                                    @elseif ($sell->status == 6)
-                                        <span class="legend-indicator bg-primary"></span>@lang("Trade Refunded")
+                                    @if ($topup->status == 0)
+                                        <span class="legend-indicator bg-warning"></span>@lang("Pending")
+                                    @elseif ($topup->status == 1)
+                                        <span class="legend-indicator bg-success"></span>@lang("Completed")
+                                    @elseif ($topup->status == 2)
+                                        <span class="legend-indicator bg-danger"></span>@lang("Cancelled")
                                     @endif
                                 </div>
-
                             </div>
                             <div class="card-body mt-2">
                                 <div class="col-sm">
                                     <!-- List Checked -->
                                     <ul class="list-checked list-checked-lg list-checked-soft-bg-primary">
                                         <li class="list-checked-item">@lang('Trx ID') : <strong
-                                                class="text-dark font-weight-bold">{{$sell->utr}}</strong></li>
-                                        <li class="list-checked-item">@lang('Exchange Rate') : <strong
-                                                class="text-dark font-weight-bold">1 {{optional($sell->sendCurrency)->code}}
-                                                ~ {{number_format($sell->exchange_rate,2)}} {{optional($sell->getCurrency)->code}}</strong>
+                                                class="text-dark font-weight-bold">{{$topup->utr}}</strong></li>
+                                        <li class="list-checked-item">@lang('Service Fees') : <strong
+                                                class="text-dark font-weight-bold">{{rtrim(rtrim(getAmount($topup->service_fee,8),0),'.')}} {{optional($topup->currency)->code}}</strong>
                                         </li>
-                                        <li class="list-checked-item">@lang('Processing Fees') : <strong
-                                                class="text-dark font-weight-bold"
-                                                id="serviceFee">{{number_format($sell->processing_fee,2)}} {{optional($sell->getCurrency)->code}}</strong>
+                                        <li class="list-checked-item">@lang('Network Fees') : <strong
+                                                class="text-dark font-weight-bold">{{rtrim(rtrim(getAmount($topup->network_fee,8),0),'.')}} {{optional($topup->currency)->code}}</strong>
                                         </li>
                                         <li class="list-checked-item">@lang('Payment Method')
-                                            : {{optional($sell->cryptoMethod)->name}}
-                                            @if(optional($sell->cryptoMethod)->is_automatic)
+                                            : {{optional($topup->method)->name}}
+                                            @if(optional($topup->method)->is_automatic)
                                                 <span
                                                     class="badge bg-soft-success text-success">@lang("Automatic")</span>
                                             @else
@@ -84,12 +71,9 @@
                                             @endif
                                         </li>
                                         <li class="list-checked-item">@lang('Requester') : <a
-                                                href="{{$sell->user_id ? route('admin.user.edit',$sell->user_id) : 'javascript:void(0)'}}">{{optional($sell->user)->fullname??'Anonymous'}}</a>
+                                                href="{{$topup->user_id ? route('admin.user.edit',$topup->user_id) : 'javascript:void(0)'}}">{{optional($topup->user)->fullname??'Anonymous'}}</a>
                                         </li>
                                     </ul>
-                                    <div class="alert alert-soft-secondary" role="alert">
-                                        @lang("Please ensure that you have received the cryptocurrency before finalizing the exchange.")
-                                    </div>
                                     <!-- End List Checked -->
                                 </div>
                             </div>
@@ -101,32 +85,24 @@
                         <div class="card">
                             <div class="card-header d-flex justify-content-between">
                                 <h4 class="card-title mt-2">@lang("Currency Information's")</h4>
-                                <span>{{dateTime($sell->created_at,basicControl()->date_time_format)}}</span>
+                                <span>{{dateTime($topup->created_at,basicControl()->date_time_format)}}</span>
                             </div>
-                            <div class="card-body mt-2" id="autoRate">
+                            <div class="card-body mt-2">
                                 <div class="col-sm">
                                     <ul class="list-checked list-checked-lg list-checked-soft-bg-secondary">
-                                        <li class="list-checked-item">@lang('Send Currency') : <strong
-                                                class="text-dark font-weight-bold">{{optional($sell->sendCurrency)->currency_name}}</strong>
+                                        <li class="list-checked-item">@lang('Currency') : <strong
+                                                class="text-dark font-weight-bold">{{optional($topup->currency)->currency_name}}</strong>
                                         </li>
-                                        <li class="list-checked-item">@lang('Receive Currency') : <strong
-                                                class="text-dark font-weight-bold">{{optional($sell->getCurrency)->currency_name}}</strong>
-                                        </li>
-                                        <li class="list-checked-item">@lang('Send Amount') : <strong
-                                                class="text-dark font-weight-bold">{{rtrim(rtrim(getAmount($sell->send_amount,8),0),'.')}} {{optional($sell->sendCurrency)->code}}</strong>
-                                        </li>
-                                        <li class="list-checked-item">@lang('Receive Amount') : <strong
-                                                class="text-dark font-weight-bold"
-                                                id="receiveAmount">{{number_format($sell->get_amount,2)}} {{optional($sell->getCurrency)->code}}</strong>
+                                        <li class="list-checked-item">@lang('Amount') : <strong
+                                                class="text-dark font-weight-bold">{{rtrim(rtrim($topup->amount,0),'.')}} {{optional($topup->currency)->code}}</strong>
                                         </li>
                                         <li class="list-checked-item">@lang('Payable Amount') : <strong
-                                                class="text-danger font-weight-bold"
-                                                id="payableAmount">{{number_format($sell->final_amount,2)}} {{optional($sell->getCurrency)->code}}</strong>
+                                                class="text-danger font-weight-bold">{{rtrim(rtrim(getAmount($topup->final_amount,8),0),'.')}} {{optional($topup->currency)->code}}</strong>
                                         </li>
                                     </ul>
                                 </div>
                                 <div class="alert alert-soft-secondary" role="alert">
-                                    @lang("The processing fee are already included in the displayed payable amount.")
+                                    @lang("The service fee and network fee are already included in the displayed payable amount.")
                                 </div>
                             </div>
                         </div>
@@ -141,44 +117,18 @@
                             <div class="card-body mt-2">
                                 <div class="col-sm">
                                     <ul class="list-checked list-checked-lg list-checked-soft-bg-warning">
-                                        <li class="list-checked-item">@lang('Admin Receive Address')
-                                            ({{optional($sell->sendCurrency)->code}}):
+                                        <li class="list-checked-item">@lang('Destination address')
+                                            ({{optional($topup->currency)->code}}) :
                                             <a href="javascript:void(0)"
-                                               onclick="copyReceiveAddress()"
+                                               onclick="copyDestinationAddress()"
                                                data-bs-toggle="tooltip"
                                                data-bs-placement="top"
                                                title="@lang("copy to clipboard")"><i
-                                                    class="fas fa-copy"></i></a>
-                                            <strong class="text-dark font-weight-bold"
-                                                    id="receiveId">{{$sell->admin_wallet}}</strong>
+                                                    class="fas fa-copy"></i></a><strong
+                                                class="text-dark font-weight-bold"
+                                                id="destinationId">{{$topup->wallet}}</strong>
                                         </li>
-                                        @if($sell->refund_wallet)
-                                            <li class="list-checked-item">@lang('Refund Address')
-                                                ({{optional($sell->sendCurrency)->code}}):
-                                                <a
-                                                    href="javascript:void(0)"
-                                                    onclick="copyRefundAddress()" data-bs-toggle="tooltip"
-                                                    data-bs-placement="top" title="@lang("copy to clipboard")"><i
-                                                        class="fas fa-copy"></i></a>
-                                                <strong
-                                                    class="text-dark font-weight-bold"
-                                                    id="refundId">{{$sell->refund_wallet}}</strong>
-                                            </li>
-                                        @endif
-                                        <li class="list-checked-item">@lang('Fiat Send Gateway') : <strong
-                                                class="text-dark font-weight-bold">{{optional($sell->fiatSendGateway)->name}}</strong>
-                                        </li>
-                                        @if($sell->parameters)
-                                            @foreach($sell->parameters as $key => $param)
-                                                <li class="list-checked-item">{{$param->field_label}} : <strong
-                                                        class="text-dark font-weight-bold">{{$param->field_value}}</strong>
-                                                </li>
-                                            @endforeach
-                                        @endif
                                     </ul>
-                                </div>
-                                <div class="alert alert-soft-secondary" role="alert">
-                                    @lang("You can commence the transfer of fiat currency by furnishing the necessary information details.")
                                 </div>
                             </div>
                         </div>
@@ -187,7 +137,6 @@
             </div>
         </div>
     </div>
-
     <div class="modal fade" id="confirmation" data-bs-backdrop="static" tabindex="-1" role="dialog"
          aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
@@ -203,63 +152,41 @@
                     <button type="button" class="btn btn-white" data-bs-dismiss="modal">@lang('Close')</button>
                     <form action="" method="post" class="deleteModalRoute">
                         @csrf
-                        @if(optional($sell->cryptoMethod)->code == 'coin_payment')
+                        @if(optional($topup->method)->code == 'coin_payment')
                             <button type="submit" name="btnValue" class="btn btn-soft-primary"
-                                    value="automatic">@lang('Send Automatic')</button>
+                                    value="automatic">@lang('Complete Automatic')</button>
                         @endif
-                        <button type="submit" name="btnValue" value="manual"
-                                class="btn btn-soft-success">@lang('Yes')</button>
+                        <button type="submit" name="btnValue" class="btn btn-soft-success"
+                                value="manual">@lang('Yes')</button>
                     </form>
                 </div>
             </div>
         </div>
     </div>
-
 @endsection
-
-
 @push('css-lib')
 @endpush
-
 @push('js-lib')
 @endpush
-
 @push('script')
     <script>
         'use strict';
-
-        $(document).on("click", "#send", function () {
-            let route = "{{route("admin.sellSend",$sell->utr)}}";
-            $("#deleteModalHeader").text(`Send Confirmation`);
-            $("#deleteModalBody").text(`Do you wish to proceed with finalizing the exchange?`);
+        $(document).on("click", "#complete", function () {
+            let route = "{{route("admin.topupComplete",$topup->utr)}}";
+            $("#deleteModalHeader").text(`Complete Confirmation`);
+            $("#deleteModalBody").text(`Do you wish to proceed with completing the top up?`);
             $(".deleteModalRoute").attr('action', route);
         });
-
         $(document).on("click", "#cancel", function () {
-            let route = "{{route("admin.sellCancel",$sell->utr)}}";
+            let route = "{{route("admin.topupCancel",$topup->utr)}}";
             $("#deleteModalHeader").text(`Cancel Confirmation`);
-            $("#deleteModalBody").text(`Do you wish to proceed with cancel the exchange?`);
+            $("#deleteModalBody").text(`Do you wish to proceed with cancel the top up?`);
             $(".deleteModalRoute").attr('action', route);
         });
-
-        $(document).on("click", "#refund", function () {
-            let route = "{{route("admin.sellRefund",$sell->utr)}}";
-            $("#deleteModalHeader").text(`Refund Confirmation`);
-            $("#deleteModalBody").text(`Do you wish to proceed with refund the exchange?`);
-            $(".deleteModalRoute").attr('action', route);
-        });
-
-
-        function copyRefundAddress() {
-            var textToCopy = document.getElementById('refundId').innerText;
+        function copyDestinationAddress() {
+            var textToCopy = document.getElementById('destinationId').innerText;
             copyExe(textToCopy);
         }
-
-        function copyReceiveAddress() {
-            var textToCopy = document.getElementById('receiveId').innerText;
-            copyExe(textToCopy);
-        }
-
         function copyExe(textToCopy) {
             var tempTextArea = document.createElement('textarea');
             tempTextArea.value = textToCopy;
@@ -269,6 +196,5 @@
             document.body.removeChild(tempTextArea);
             Notiflix.Notify.success('Text copied to clipboard: ' + textToCopy);
         }
-
     </script>
 @endpush
